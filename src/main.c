7aef725137c7357e529ea527a5f337e45d9fcbf5
Emilie <Emilie@DESKTOP-0UBRP4U>
2017-10-27 09:57:18 +0200

    initial commit

---
+++ src/main.c
@@ -0,0 +1,384 @@
+/**
+*****************************************************************************
+**
+**  File        : main.c
+**
+**  Abstract    : main function.
+**
+**  Functions   : main
+**
+**  Environment : Atollic TrueSTUDIO(R)
+**                STMicroelectronics STM32F4xx Standard Peripherals Library
+**
+**  Distribution: The file is distributed “as is,” without any warranty
+**                of any kind.
+**
+**  (c)Copyright Atollic AB.
+**  You may use this file as-is or modify it according to the needs of your
+**  project. Distribution of this file (unmodified or modified) is not
+**  permitted. Atollic AB permit registered Atollic TrueSTUDIO(R) users the
+**  rights to distribute the assembled, compiled & linked contents of this
+**  file as part of an application binary file, provided that it is built
+**  using the Atollic TrueSTUDIO(R) toolchain.
+**
+**
+*****************************************************************************
+*/
+
+/* Includes */
+#include "stm32f4xx.h"
+#include "stm32f4_discovery.h"
+#include "math.h"
+/* Private macro */
+#define AUDIOBUFFERSIZE         2500
+#define WAVEFREQ            	880 	/* 880Hz --> A5 */
+#define TIMER6_PRESCALER    	2     	/* produces a 42MHz tick */
+#define TIMER_CLOCK           	84E6 	/* TIM6 runs at 84MHz */
+
+/* Private variables */
+uint16_t silenceBuffer[AUDIOBUFFERSIZE] = {0};
+uint16_t AUDIOBuffer[AUDIOBUFFERSIZE];     /* Array for the waveform */
+uint16_t AUDIOBuffer1[AUDIOBUFFERSIZE];
+uint8_t beatFlag = 0;
+uint8_t buttonPress = 1;
+/* Private function prototypes */
+/* Private functions */
+void RCC_Configuration(void);
+void DMA_Configuration(uint16_t * waveBuffer);
+void DMA_ChangeBuffer(uint16_t *waveBuffer);
+void NVIC_Configuration(void);
+void GPIO_Configuration(void);
+void UART_Configuration(void);
+void Timer_Configuration(uint16_t wavPeriod, uint16_t preScaler);
+void DAC_Configuration(void);
+void delay_ms(uint32_t milli);
+
+/*******************************************************************************
+* Function Name  : TIM2_IRQHandler
+* Description    : This function handles TIM2 global interrupt request.
+* Input          : None
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void TIM2_IRQHandler(void)
+{
+  if (TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET)
+  {
+    TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+
+    // for now, timer2 shouldn't do anything, though we might want to use it later to do something,
+    // so I am leaving it here for now (uncomment the set up in Timer_Configuration)
+    STM_EVAL_LEDToggle(LED4);
+    beatFlag = 1;
+  }
+}
+
+
+/**
+**===========================================================================
+**
+**  Abstract: main program
+**
+**===========================================================================
+*/
+int main(void)
+{
+    uint32_t fTimer;
+    uint32_t timerFreq;
+    uint16_t timerPeriod;
+    uint8_t buffNum = 1;
+
+    STM_EVAL_LEDInit(LED3);
+    STM_EVAL_LEDInit(LED4);
+    STM_EVAL_LEDInit(LED5);
+    STM_EVAL_LEDInit(LED6);
+
+
+    /* Create wave table for example sin() wave */
+    for (uint16_t n = 0; n < AUDIOBUFFERSIZE; n++)
+    {
+    	AUDIOBuffer1[n] = 0.10*(uint16_t)((0xFFF+1)/2)*sin((2*M_PI*n)/AUDIOBUFFERSIZE+1); /* Remember to cast! */
+    }
+
+    /* Create wave table using a specified frequency */
+    for (uint16_t n = 0; n < 19919; n++)
+	{
+    	// TODO: Fix this equation cause it's really not working lol
+    	long double exp = -20*(n*6.25E-5);
+    	long double power = pow(M_E, exp);
+    	AUDIOBuffer[n] = (uint16_t)((0xFFF+1)/2)*sin((2*M_PI*n*50)/48000+1);
+    	AUDIOBuffer[n] = AUDIOBuffer[n]*power;
+	}
+
+    /* Calculate frequency of timer */
+    fTimer = WAVEFREQ * AUDIOBUFFERSIZE;
+
+    /* Calculate Tick Rate */
+    timerFreq = TIMER_CLOCK / TIMER6_PRESCALER; /* Timer tick is in Hz */
+
+    /* Calculate period of Timer */
+    timerPeriod = (uint16_t)( timerFreq / fTimer );
+
+    /* System Clocks Configuration */
+    RCC_Configuration();
+
+    /* NVIC configuration */
+    NVIC_Configuration();
+
+    /* Configure the GPIO ports */
+    GPIO_Configuration();
+
+    /* Timer Configuration */
+    Timer_Configuration( timerPeriod, TIMER6_PRESCALER );
+
+    /* DAC Configuration */
+    DAC_Configuration();
+
+    /* DMA Config */
+    DMA_Configuration(AUDIOBuffer);
+
+    //init the push button
+    STM_EVAL_PBInit(BUTTON_USER, BUTTON_MODE_GPIO);
+
+    while (1) {
+    	// NOTE from Em: This should preferably be an empty while loop
+    	// interrupts should handle everything
+
+    	// check button to allow toggle on and off
+    	if (STM_EVAL_PBGetState(BUTTON_USER) == Bit_SET) {
+    		/* Debounce */
+			while(STM_EVAL_PBGetState(BUTTON_USER) == Bit_SET);
+			DMA_ChangeBuffer(silenceBuffer); // NOTE from Em: This doesn't actually seem to work
+    	} else {
+    		//check for beat flag if buttonPressed
+			if(beatFlag == 1){
+
+				STM_EVAL_LEDToggle(LED3);
+
+				/* Configure Saw Tooth */
+				if (buffNum == 1) {
+					DMA_ChangeBuffer(AUDIOBuffer1);
+					buffNum = 2;
+				}
+
+				/* Configure Sine Wave */
+				else {
+					DMA_ChangeBuffer(AUDIOBuffer);
+					buffNum = 1;
+				}
+
+				beatFlag = 0;
+			}
+    	}
+    }
+}
+
+/**
+  * @brief  Configures the different system clocks.
+  * @param  None
+  * @retval : None
+  */
+void RCC_Configuration(void)
+{
+    /* Enable DMA and GPIOA Clocks */
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA1 | RCC_AHB1Periph_GPIOA, ENABLE);
+
+    /* Enable DAC1 and TIM6 & TIM2 clocks */
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_DAC | RCC_APB1Periph_TIM6 | RCC_APB1Periph_TIM2, ENABLE);
+}
+
+/**
+  * @brief  Configures the nested vectored interrupt controller.
+  * @param  None
+  * @retval : None
+  */
+void NVIC_Configuration(void)
+{
+	//NOTE (from Luke): What I think this doing is setting up the TIM2_IRQHandler as above.
+	// This is important to remember when setting up a timer that needs a handler
+    NVIC_InitTypeDef NVIC_InitStructure;
+    NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+
+    NVIC_Init(&NVIC_InitStructure);
+
+    /* Enable DMA1 Stream5 IRQ Channel */
+    NVIC_InitStructure.NVIC_IRQChannel = DMA1_Stream5_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+}
+
+
+/**
+  * @brief  Configures the different GPIO ports.
+  * @param  None
+  * @retval : None
+  */
+void GPIO_Configuration(void)
+{
+	//NOTE (from Luke): I believe this is setting up pin A4  (PA4) as the audio output
+
+    GPIO_InitTypeDef GPIO_InitStruct;
+
+    /* Pack the struct */
+    GPIO_InitStruct.GPIO_Speed = GPIO_Mode_AN;
+    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_4;
+    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+
+    /* Call Init function */
+    GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+}
+
+/* Function to change DMA Buffer Array */
+void DMA_ChangeBuffer(uint16_t *waveBuffer){
+	DMA_Cmd(DMA1_Stream5, DISABLE);
+	DMA_Configuration(waveBuffer);
+}
+
+/* DMA Interrupt Handler */
+void DMA1_Stream5_IRQHandler(void)
+{
+  //Test on DMA1 Channel 7 Transfer Complete interrupt
+  if(DMA_GetITStatus(DMA1_Stream5, DMA_IT_TCIF5) != RESET)
+  {
+    // TODO: Handle Transfer Complete Callback
+	//Clear DMA1 Channel 7 Transfer Complete interrupt pending bits
+    DMA_ClearITPendingBit(DMA1_Stream5, DMA_IT_TCIF5);
+  }
+}
+
+
+/**
+  * @brief  Configures the DMA.
+  * @param  None
+  * @retval : None
+  */
+void DMA_Configuration(uint16_t * waveBuffer)
+{
+    DMA_InitTypeDef DMA_InitStructure;
+
+    //Initialize the structure to default values
+    DMA_StructInit(&DMA_InitStructure);
+
+    /* PACK YOUR INIT STRUCT HERE */
+    DMA_InitStructure.DMA_Channel = DMA_Channel_7;								//the channel we are using
+    DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)(DAC_BASE + 0x08);		//what peripheral we are linking the DMA to
+    DMA_InitStructure.DMA_Memory0BaseAddr = (uint32_t)(waveBuffer);			//what data we are linking to the DMA
+    DMA_InitStructure.DMA_DIR = DMA_DIR_MemoryToPeripheral;						//the direction things must flow: memory to peripheral
+    DMA_InitStructure.DMA_BufferSize = (uint32_t)AUDIOBUFFERSIZE;				//the size, or length of the data
+    DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;			//We dont want to change the location of the peripheral we send to, so we disable the peripheral address increment
+    DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;						//We want to increment the address of the data we are sening (else we send the same segment of data over and o
+    DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord; //The size of the peripherals data size (so how much the peripheral can accept at one time)
+    DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord;			//The size of the memory. 16 bits. This should be the same as above, i guess?
+    DMA_InitStructure.DMA_Mode = DMA_Mode_Circular;								//TODO: This probably needs to change to Normal so the data is only played once
+    DMA_InitStructure.DMA_Priority = DMA_Priority_High;							//High prio?
+    DMA_InitStructure.DMA_FIFOMode = DMA_FIFOMode_Disable;						//? - dont think it is important
+    DMA_InitStructure.DMA_FIFOThreshold = DMA_FIFOThreshold_HalfFull;			//? - dont think it is important
+    DMA_InitStructure.DMA_MemoryBurst = DMA_MemoryBurst_Single;					//? - dont think it is important
+    DMA_InitStructure.DMA_PeripheralBurst = DMA_PeripheralBurst_Single;			//? - dont think it is important
+    /* Call Init function */
+    DMA_Init(DMA1_Stream5, &DMA_InitStructure);
+
+    /* Enable the Transfer Complete Interrupt */
+	DMA_ITConfig(DMA1_Stream5, DMA_IT_TC, ENABLE);
+    /* Enable DMA */
+    DMA_Cmd(DMA1_Stream5, ENABLE);
+
+}
+
+/**
+  * @brief  Configures the Timers.
+  * @param  wavePeriod (period of timer), preScaler (prescaler for timer)
+  * @retval : None
+  */
+void Timer_Configuration(uint16_t wavPeriod, uint16_t preScaler)
+{
+    TIM_TimeBaseInitTypeDef  TIM_TimeBaseStruct;
+
+    /* pack Timer struct */
+    TIM_TimeBaseStruct.TIM_Period = wavPeriod-1; // should be <count frequency>/<wave res>*<audio frequency>
+    TIM_TimeBaseStruct.TIM_Prescaler = preScaler-1;
+    TIM_TimeBaseStruct.TIM_ClockDivision = TIM_CKD_DIV1;
+    TIM_TimeBaseStruct.TIM_CounterMode = TIM_CounterMode_Up;
+    TIM_TimeBaseStruct.TIM_RepetitionCounter = 0x0000;
+
+    /* Call init function */
+    TIM_TimeBaseInit(TIM6, &TIM_TimeBaseStruct);
+
+    /* Select Timer to trigger DAC */
+    TIM_SelectOutputTrigger(TIM6, TIM_TRGOSource_Update);
+
+    /* TIM6 enable counter */
+    TIM_Cmd(TIM6, ENABLE);
+
+    /* TIM2 Set Up */ //commented out for now, see the handler above
+    TIM_TimeBaseStruct.TIM_Period = 84000-1;
+    TIM_TimeBaseStruct.TIM_Prescaler = 1000-1;
+    TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStruct);
+    TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
+    TIM_Cmd(TIM2, ENABLE);
+}
+
+/**
+  * @brief  Configures the DAC
+  * @param  None
+  * @retval : None
+  */
+void DAC_Configuration(void)
+{
+    DAC_InitTypeDef DAC_InitStruct;
+
+    /* Initialize the DAC_Trigger member */
+    DAC_InitStruct.DAC_Trigger = DAC_Trigger_T6_TRGO;
+    /* Initialize the DAC_WaveGeneration member */
+    DAC_InitStruct.DAC_WaveGeneration = DAC_WaveGeneration_None;
+    /* Initialize the DAC_LFSRUnmask_TriangleAmplitude member */
+    DAC_InitStruct.DAC_LFSRUnmask_TriangleAmplitude = DAC_LFSRUnmask_Bit0;
+    /* Initialize the DAC_OutputBuffer member */
+    DAC_InitStruct.DAC_OutputBuffer = DAC_OutputBuffer_Enable;
+
+    /* Init DAC */
+    DAC_Init(DAC_Channel_1, &DAC_InitStruct);
+
+    /* Enable DMA request */
+    DAC_DMACmd(DAC_Channel_1, ENABLE);
+
+    /* Enable DAC Channel1: Once the DAC channel1 is enabled, PA.04 is automatically connected to the DAC converter. */
+    DAC_Cmd(DAC_Channel_1, ENABLE);
+
+}
+
+
+void delay_ms(uint32_t milli)
+{
+	uint32_t delay = milli * 17612;              // approximate loops per ms at 168 MHz, Debug config
+	for(; delay != 0; delay--);
+}
+
+
+//These have to be here for some reason. I deleted them and then it wouldnt build :C
+
+/*
+ * Callback used by stm32f4_discovery_audio_codec.c.
+ * Refer to stm32f4_discovery_audio_codec.h for more info.
+ */
+void EVAL_AUDIO_TransferComplete_CallBack(uint32_t pBuffer, uint32_t Size){
+  /* TODO, implement your code here */
+  return;
+}
+
+/*
+ * Callback used by stm324xg_eval_audio_codec.c.
+ * Refer to stm324xg_eval_audio_codec.h for more info.
+ */
+uint16_t EVAL_AUDIO_GetSampleCallBack(void){
+  /* TODO, implement your code here */
+  return -1;
+}
